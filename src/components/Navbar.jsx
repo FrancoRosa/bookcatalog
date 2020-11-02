1bfaeb0a8cd42e1b1375c4d7ea4486dc00e22394
FrancoRosa <km115.franco@gmail.com>
2020-11-02 15:15:10 -0500

    Add style sheet and style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const displayItems = () => {
+  const menu = document.querySelector('.navbar-menu');
+  menu.classList.toggle('is-active');
+};
+
 const Navbar = () => (
-  <ul>
-    <li><Link to="/books">Catalog</Link></li>
-    <li><Link to="/about">About</Link></li>
-  </ul>
+  <nav className="navbar">
+    <div className="navbar-brand">
+      <p className="navbar-item title">BooksCatalog</p>
+      <a onClick={displayItems} href="#0" role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+    <div className="navbar-menu">
+      <div className="navbar-start">
+        <Link className="navbar-item" to="/books">Catalog</Link>
+        <Link className="navbar-item" to="/about">About</Link>
+      </div>
+
+    </div>
+  </nav>
 );
 
 export default Navbar;
