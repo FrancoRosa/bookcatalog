97b10bca9a761893fb0ffa8e092a6154420f4486
FrancoRosa <km115.franco@gmail.com>
2020-11-02 11:16:34 -0500

    Add linter config files

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux';
 
 const BookInfo = ({match, books})  => {
-  let book = books.filter(book => book.author === match.params.id)
+  const book = books.filter(book => book.author === match.params.id)
   if (book.length === 0) return <p>Book not found. Return to catalog</p> 
-  
-  book = book[0];
+
+  const {title, author, description, publisher, contributor, isbns} = book[0]
   return (
     <div>
-      <h2>{book.title}</h2>
-      <p>{book.author} </p>
-      <p>Author</p>
-      <p>etc.</p>
-      {console.log('>>><this book')}
-      {console.log(books)}
-      {console.log(match.params.id)}
+      <h2>{title}</h2>
+      <p>Author: {author} </p>
+      <p>Contributor: {contributor} </p>
+      <p>Description: {description} </p>
+      <p>Publisher: {publisher} </p>
+      <p>ISBN: {isbns[0].isbn10}, {isbns[0].isbn10} </p>
     </div>
   );
 };
